c866a422b491cccc22234506df325ad05a6ff5e4
sebwe <sebwe@live.dk>
2025-04-26 12:46:28 +0200

    g

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/pokemon-pokedex/',
+  plugins: [react(), ghPages()],
+  base: '/pokemon-pokedex/', // <-- sørg for at ændre dette til dit repo-navn
 })
